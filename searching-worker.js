f87ffbe431963802b66e51b6f72f2f51056a0114
Renaud Genevois <KQHLZ2@ad.insee.intra>
2021-07-05 10:50:12 +0200

    search param in orker

--- searching-worker.js
+++ searching-worker.js
@@ -3,8 +3,8 @@ import 'regenerator-runtime/runtime';
 import searching from './src/searching';
 
 self.onmessage = function (e) {
-  const { searh, name, version, max } = e.data;
-  searching(searh, name, version, max).then(function (result) {
+  const { search, name, version, max } = e.data;
+  searching(search, name, version, max).then(function (result) {
     self.postMessage(result);
   });
 };
