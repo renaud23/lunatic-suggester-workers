c2069ff531617485fca83adfbe036928d9d3bfc7
renaud23 <renaud.genevois23@gmail.com>
2021-09-01 11:04:47 +0200

    search test

--- src/stories/compute-score.stories.js
+++ src/stories/compute-score.stories.js
@@ -4,12 +4,15 @@ import clearStore from '../commons-idb/clear-store';
 import appendStore from '../store-index/append';
 import insertEntity from '../commons-idb/insert-entity';
 import * as CONSTANTES from '../commons-idb/constantes';
+import searching from '../searching';
 import { storeInfo, fetchNafRev2 } from './naf-rev2';
+import { doUntil } from 'async';
 
 const idbVersion = '1';
 
 export function ComputeScore() {
   const [db, setDb] = useState(undefined);
+  const [search, setSearch] = useState('cultur taba');
   useEffect(function () {
     async function init() {
       setDb(await createStore(storeInfo.name, idbVersion));
@@ -33,9 +36,25 @@ export function ComputeScore() {
     [db]
   );
 
+  const onSearch = useCallback(
+    function () {
+      async function doIt() {
+        const results = await searching(search, storeInfo.name, '1');
+        console.log(results);
+      }
+
+      doIt();
+    },
+    [search]
+  );
+
   return (
     <>
       <input type="button" onClick={fillStore} value="Fill Store!" />
+      <div>
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+        <input type="button" value="Search" onClick={onSearch} />
+      </div>
     </>
   );
 }
